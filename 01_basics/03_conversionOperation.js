e8aa1535c6e50590d74357f480a15b71030d2de5
devsharma0007 <dev716415@gmail.com>
2026-08-27 19:10:42 +0000

    more about conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = undefined 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 //"33" = 33
 // "33abc" = NaN
@@ -17,7 +17,7 @@ let isLoggedIn  = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 = true; 0 = false;
 
@@ -25,5 +25,21 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
+let str1 = "hello"
+let str2 = "dev"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+1+"2");
+
+let a = 3;
+let b = a++;
+console.log(a,b);
+
+a = 3;
+b = ++a;
+console.log(a,b);
