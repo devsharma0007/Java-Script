3d2da75fb149ba9f3c3aacc0f4462eb73a0fff43
devsharma0007 <dev716415@gmail.com>
2026-08-31 21:12:07 +0000

    Datatype Summary

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+const heros = ["IronMan","Thor","CaptionAmerica"]
+
+const myobj = {
+    name : "dev",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
+console.log(typeof heros);
