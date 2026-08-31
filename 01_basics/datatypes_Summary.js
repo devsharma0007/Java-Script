b2878b07fac0ff62ac033b91bf8cb684c5b8abcb
devsharma0007 <dev716415@gmail.com>
2026-08-31 21:46:09 +0000

    datatypes&Summary

--- 01_basics/datatypes_Summary.js
+++ 01_basics/datatypes_Summary.js
@@ -10,3 +10,31 @@ const myFunction = function(){
 }
 
 console.log(typeof heros);
+
+
+// +++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack(Primitive), Heap(Non-Primitive)
+ 
+let myname = "dev"
+let anothername = myname 
+anothername = "ourname"
+
+console.log(myname)
+console.log(anothername);
+
+let userOne ={
+    email : "user&google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+// userTwo = {
+//     email : "user2&google.com",
+//     upi : "user2@ybl"
+// }
+userTwo.email = "nitin@yahoo.com"
+
+console.log(userOne.email);
