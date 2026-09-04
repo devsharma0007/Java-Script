e8887e9c29c6dbdde3b584c082768ac479fdc8e6
devsharma0007 <dev716415@gmail.com>
2026-09-04 20:00:23 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocalString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2023,0,23);
+
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
