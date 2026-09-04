1c801df0d6aadc3abe096063b6bbe437afad5c5a
devsharma0007 <dev716415@gmail.com>
2026-09-04 19:32:46 +0000

    nums and maths

--- 01_basics/06_nums_maths.js
+++ 01_basics/06_nums_maths.js
@@ -17,5 +17,17 @@
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // ++++++++++++++ Maths ++++++++++++++//
+console.log(Math.random());
+console.log((Math.random()*10)+1);
 
-console.log(Math);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 20;
+const max = 30;
+
+console.log(Math.floor(Math.random() * (max-min +1) + min));
+
+const min2 = 40;
+const max2 = 100;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
