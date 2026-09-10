304232e4e5f9ac7da13cc17748473806f0bb2e45
devsharma0007 <dev716415@gmail.com>
2026-09-10 21:36:21 +0000

    objects and functions

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,8 +1,28 @@
-// object literals
+// object literals 
+const mysym = Symbol("key1")
 
-const JsUser = {
-    name : "Dev"
+const jsUser = {
+    name : "DKS",
+    age : 21,
+    [mysym] : "mykey1",
+    location : "Faridabad",
+    email : "dev716415@gmail.com"  
+}
+console.log(jsUser["age"]);
+console.log(jsUser[mysym]);
 
+jsUser.email = "dev716414@gmail.com"
+// Object.freeze(jsUser)
+jsUser.email = "arunsharma12487@gmail.com"
+
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello JS user");
 }
 
-myArray = [""]
+jsUser.greeting2 = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+jsUser.greeting();
+jsUser.greeting2();
