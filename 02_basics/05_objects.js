222b6caa3004e1e684cc9857fcf662dd8dfbacc2
devsharma0007 <dev716415@gmail.com>
2026-09-14 20:09:08 +0000

    objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename : "java script",
+    price : "999",
+    courseInstructor : "Dev Sharma"
+}
+
+const {courseInstructor : instructor} = course
+
+console.log(instructor);
+
+// {
+//     "name" : "Dev",
+//     "coursename" : "Java-Script",
+//     "price" : "1000"
+// }
+
+[
+    {},
+    {},
+    {}
+]
